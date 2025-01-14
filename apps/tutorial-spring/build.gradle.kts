31d3cd7344c0686a7bb8a726637afe7a844036b2
kkihwan22 <jinkh97@gmail.com>
2025-01-14 16:49:18 +0900

    feat. kafka-streams

--- apps/tutorial-spring/build.gradle.kts
+++ apps/tutorial-spring/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 dependencies {
     implementation(Spring.boot.web)
     implementation(Spring.kafka)
+    implementation("org.apache.kafka:kafka-streams")
+    implementation("com.jayway.jsonpath:json-path:2.7.0")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
