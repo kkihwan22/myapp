2904091fec65bf003a4cab30c99f10e3c0f5d60c
kkihwan22 <jinkh97@gmail.com>
2025-01-17 17:16:21 +0900

    config. com.jayway.jsonpath:json-path 라이브러리 버전을 refreshVersion에서 관리하도록 변경

--- apps/tutorial-spring/build.gradle.kts
+++ apps/tutorial-spring/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 dependencies {
     implementation(Spring.boot.web)
-    implementation(Spring.session.dataRedis)
     implementation(Spring.kafka)
+    implementation(Spring.session.dataRedis)
     implementation(Spring.boot.data.redis)
 
     implementation("org.apache.kafka:kafka-streams")
-    implementation("com.jayway.jsonpath:json-path:2.7.0")
+    implementation("com.jayway.jsonpath:json-path:_")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
